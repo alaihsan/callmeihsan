b5a892fa825090890d7a9df874d2d47d9e13d7f4
muhamad ikhsan <iksan.qren@gmail.com>
2022-05-05 08:24:26 +0700

    add styled component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Divi = styled.div `
+background-color: rgba(0, 22 , 100, 0.5);
+padding: 10px;
+border-radius: 4%`;
+
+const Paragraph1 = styled.p `
+font-size: 3em;
+color: white;
+text-align: center;
+`;
 
 export default function About() {
     return (
         <>
+        <Divi>
+            <Paragraph1>
+                haiii red
+            </Paragraph1>
+        </Divi>
         </>
     );
 }
